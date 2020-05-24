594d7f4c293cdfd0826b04975c5b29e998f0d33f
Y-Zett <fedchun.vas@gmail.com>
2020-05-25 00:12:44 +0300

    NewS

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FourCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int N = Convert.ToInt32(Console.ReadLine());
+            N += 5;
+            Console.WriteLine(Convert.ToString(N));
+
+            for (int i = 0; i < 5; i++)
+            {
+                N = 1;
+            }
+        }
+
+    }
+}
