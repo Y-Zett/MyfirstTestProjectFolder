5ba155b61c450ea1c950e070ff6e893664aec9ac
Y-Zett <fedchun.vas@gmail.com>
2020-05-25 19:03:30 +0300

    so so

--- Program.cs
+++ Program.cs
@@ -1,28 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FourCs
 {
     class Program
-    {
+    {   
+        
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            int N = Convert.ToInt32(Console.ReadLine());
-            N += 5;
-            Console.WriteLine(Convert.ToString(N));
+        {   
+            List<Tuple<int, int>> numbers = new List<Tuple<int, int>>();
+           
+            
+            long N = numbers[0];
+            long M = numbers[1];
+            long koord1 = numbers[2];
+            long koord2 = numbers[3];
+            numbers.Clear();
 
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Hello there");
-            }
+            
+            
 
-            for (int e = 0; e < 10; e++)
-            {
-                if (e > 5)
-                {
-                    Console.WriteLine("Hentai");
-                }
-            }
+            
         }
 
     }
